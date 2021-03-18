044e5752f9a536ded9d2d6a9153854e5918af768
정지환 <kkiu1756@gmail.com>
2021-03-18 14:24:52 +0900

    Init: 백엔드 스켈레톤 코드 업로드

---
+++ backend/src/main/java/ssafy/a306/vnc/VncApplication.java
@@ -0,0 +1,13 @@
+package ssafy.a306.vnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VncApplication.class, args);
+	}
+
+}
