aa3b813fc21ed517b5214417dc5eb83a9c86917c
이석호 <dldlsh2309@gmail.com>
2021-03-24 14:05:26 +0900

    feat : 투표 생성 기능 구현

--- backend/src/main/java/ssafy/a306/vnc/repository/RepositoryTest.java
+++ backend/src/main/java/ssafy/a306/vnc/repository/RepositoryTest.java
@@ -11,11 +11,11 @@ public class RepositoryTest {
 	
 	public void create() {
 		
-		VoteVo VoteVo = new VoteVo(3,"hihi","하하호호",3,0);
-				
-		VoteVo newVote = voteRepository.save(VoteVo);
-		
-		System.out.println(newVote);
+//		VoteVo VoteVo = new VoteVo(3,"hihi","하하호호",3,0);
+//				
+//		VoteVo newVote = voteRepository.save(VoteVo);
+//		
+//		System.out.println(newVote);
 
 	}
 }
