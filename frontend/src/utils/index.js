d4f2ba1322e4cfd59cd2d9b82f9c1f8af56e705b
정지환 <kkiu1756@gmail.com>
2021-03-31 03:59:48 +0900

    feat: 스마트 컨트랙트 데이터 요청 함수 구현
    - frontend/src/build/conrtracts: contract abi
    - frontend/src/utils/index.js: 스마트 컨트랙트 연동
    - frontend/src/config/index.js: 이더리움 네트워크 설정
    - frontend/src/views/VoteGraph.vue: 데이터 요청 함수 구현 (getData)

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,61 @@
+import Web3 from "web3";
+import {
+    contractAddress,
+    abi,
+    gethHost
+} from "../config"
+
+if (!localStorage.myData) {
+    localStorage.myData = JSON.stringify(this.web3.eth.accounts.create());
+    console.log("new account created: ", JSON.parse(localStorage.myData).address)
+}
+
+export class Utils {
+    static web3 = new Web3(gethHost);
+    static contract = new this.web3.eth.Contract(abi, contractAddress);
+    static account = JSON.parse(localStorage.myData).address;
+
+    constructor() {}
+
+    static async getBalance() {
+        return await this.web3.eth.getBalance(JSON.parse(localStorage.myData).address)
+    }
+
+    static async signAndSend(method, args) {
+        const web3 = this.web3;
+        const contract = this.contract;
+        const privateKey = JSON.parse(localStorage.myData).privateKey
+        const account = JSON.parse(localStorage.myData).address;
+
+        // 트랜잭션 데이터
+        // const txBuilder = contract.methods.addItem();
+        const txBuilder = method(...args);
+        const encodedTx = txBuilder.encodeABI();
+
+        // 기타 데이터
+        const nonce = await web3.eth.getTransactionCount(account);
+        const block = await web3.eth.getBlock("latest")
+        const gasLimit = block.gasLimit;
+        const gasPrice = await web3.eth.getGasPrice() * 1.5;
+
+        const txObject = {
+            nonce: web3.utils.toHex(nonce),
+            gasLimit,
+            gasPrice,
+            data: encodedTx,
+            from: JSON.parse(localStorage.myData).address,
+            to: contractAddress
+        }
+
+        // sign
+        web3.eth.accounts.signTransaction(txObject, privateKey, function (error, signedTx) {
+            if (error) {
+                console.log("error!", error)
+            } else {
+                // send
+                web3.eth.sendSignedTransaction(signedTx.rawTransaction).then((res) =>
+                    console.log("result", res))
+            }
+        })
+    }
+}
--- frontend/src/views/VoteGraph.vue
+++ frontend/src/views/VoteGraph.vue
@@ -1,28 +1,17 @@
 <template>
   <div>
-    <h1>
-      프로듀스 101 투표
-    </h1>
+    <h1>{{ mainTitle }}</h1>
     <div>
-      <img
-        src="https://source.unsplash.com/random"
-        alt=""
-        style="width:50%; height:30%"
-      />
+      <img :src="mainImagePath" alt="" style="width: 50%; height: 30%" />
     </div>
     <p>
-      마침내 세계가 놀랄 글로벌 아이돌을 탄생시킬 x의 베일이 벗겨진다. 글로벌
-      아이돌 육성 프로젝트! 프로듀스X 101
+      {{ mainDescription }}
     </p>
 
     <div>
       <h1>My Web Page</h1>
 
-       <GChart
-    type="PieChart"
-    :data="chartData"
-    :options="chartOptions"
-  />
+      <GChart type="PieChart" :data="chartData" :options="chartOptions" />
     </div>
 
     <div>
@@ -39,37 +28,65 @@
 
 <script>
 import FootBar from "@/components/common/FootBar";
-import { GChart } from 'vue-google-charts'
- 
+import { GChart } from "vue-google-charts";
+import { Utils } from "@/utils/index.js";
+
 export default {
   components: {
     FootBar,
-    GChart
+    GChart,
   },
   data() {
     return {
-          // Array will be automatically processed with visualization.arrayToDataTable function
+      mainTitle: "",
+      mainDescription: "",
+      mainImagePath: "",
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      loaded: false,
       chartData: [
-        ['Year', 'Sales', 'Expenses', 'Profit'],
-        ['2014', 1000, 400, 200],
-        ['2015', 1170, 460, 250],
-        ['2016', 660, 1120, 300],
-        ['2017', 1030, 540, 350]
+        ["Key", "Value"],
+        ["2014", 1000],
+        ["2015", 1170],
+        ["2016", 660],
+        ["2017", 1030],
       ],
       chartOptions: {
         chart: {
-          title: 'Company Performance',
-          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-        }
-      }
+          title: "Company Performance",
+          subtitle: "Sales, Expenses, and Profit: 2014-2017",
+        },
+      },
     };
   },
   created() {
-  google.charts.load('current', {'packages':['corechart']});
-  google.charts.setOnLoadCallback(this.drawChart());
-
+    // 오류 발생 임시 주석 처리
+    // google.charts.load("current", { packages: ["corechart"] });
+    // google.charts.setOnLoadCallback(this.drawChart());
+    this.getData(0);
   },
   methods: {
+    async getData(idx) {
+      this.web3 = Utils.web3;
+      const contract = Utils.contract;
+
+      // get vote data
+      const rs = await contract.methods.getVote(idx).call();
+      console.log(rs);
+
+      // set data
+      this.mainTitle = rs.title;
+      this.mainDescription = rs.description;
+      this.mainImagePath = rs.imagePath;
+
+      // set chart
+      this.chartData = [["Key", "Value"]];
+      rs.items.forEach((el) => {
+        this.chartData.push([el.title, el.count]);
+      });
+
+      // load complete
+      this.loaded = true;
+    },
     drawChart() {
       var data = google.visualization.arrayToDataTable([
         ["Task", "Hours per Day"],
