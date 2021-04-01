daf24e5487cb2a7c1924aefd05434c6449cbe57e
이석호 <dldlsh2309@gmail.com>
2021-04-01 18:06:01 +0900

    feat : 투표하기 페이지 구현

---
+++ frontend/src/components/votepage/ImageRadio.vue
@@ -0,0 +1,64 @@
+<template>
+  <label>
+    <input type="radio" name="test" value="small" />
+    <div id="img">
+        <h2 style="margin-left:10px">{{idx + 1}} . {{title}}</h2>
+      <img
+        src="https://source.unsplash.com/random"
+        style="margin-left:50px;margin-top:10px; width:200px;height:200px"
+      />
+      <!-- <button style="margin-left:200px;margin-top:100px">상세 보기</button> -->
+      <b-button @click="openDetail" style="margin-left:200px;margin-top:50px">상세 보기</b-button>
+        
+      <b-modal ref="detail" title="상세보기" >
+        <p class="my-4">투표 항목에 대한 정보 {{idx + 1}}</p>
+      </b-modal>
+    </div>
+  </label>
+</template>
+
+<script>
+export default {
+  props: ['idx'],
+  data() {
+    return {
+      confirm: -1,
+      title: "항목" + (this.idx+1)
+    };
+  },
+  methods: {
+    openDetail() {
+      //this.confirm = this.idx;
+      this.$refs['detail'].show()
+    },
+    closeDetail() {
+      this.confirm = -1;
+    //   $bvModal.hide('idx');
+    },
+  },
+};
+</script>
+
+<style>
+[type='radio'] {
+  position: absolute;
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* IMAGE STYLES */
+[type='radio'] + div#img {
+  cursor: pointer;
+  width: 300px;
+  height: 350px;
+  outline: 1px solid rgb(14, 12, 12);
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+/* CHECKED STYLES */
+[type='radio']:checked + div#img {
+  outline: 2px solid #f00;
+}
+</style>
--- frontend/src/views/VotePage.vue
+++ frontend/src/views/VotePage.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
-    <div class="container">
+    <div
+      class="container"
+      style="border: 1px solid rgb(14, 12, 12);margin-bottom:50px"
+    >
       <div name="title">
-        <h1>투표 제목</h1>
+        <center>
+          <h1>투표 제목</h1>
+        </center>
       </div>
-      <div name="main-image" style="width: 100%; height: 100%">
-        <img
-          src="https://source.unsplash.com/random"
-          style="width: 600px; height: 400px"
-        />
+      <div name="main-image" style="margin-top:30px">
+        <center>
+          <img
+            src="https://source.unsplash.com/random"
+            style="width: 300px; height: 200px;"
+          />
+        </center>
       </div>
       <div name="content">
-        <p>투표 내용</p>
+        <center>
+          <p style="font-size:25px;margin-top:50px;margin-bottom:50px;margin-left:50px;margin-right:50px">
+            당신의 눈앞에 선 101명의 소녀! 걸그룹 최종 멤버 선택과 데뷔 싱글의
+            프로듀싱은 모두 TV를 보고 있는 당신의 몫! 과연 당신은 어느 소녀의
+            재능과 매력에 손을 들어주게 될 것인가?
+          </p>
+        </center>
       </div>
 
       <div
@@ -23,40 +36,50 @@
           justify-content: center;
         "
       >
-        <div><VoteCard /></div>
-        <div><VoteCard /></div>
-        <div><VoteCard /></div>
-        <div><VoteCard /></div>
-        <div><VoteCard /></div>
-        <div><VoteCard /></div>
-      </div>
+      <!-- eslint-disable vue/no-use-v-if-with-v-for,vue/no-confusing-v-for-v-if -->
+         <div v-for="(item, idx) in list" v-bind:item="item" v-bind:key="idx">
+          <ImageRadio :idx="idx" />
+        </div> 
 
-      <div name="vote-list" style="width: 100%">
-        <div><VoteWritten /></div>
-        <div><VoteWritten /></div>
-        <div><VoteWritten /></div>
-        <div><VoteWritten /></div>
-        <div><VoteWritten /></div>
-        <div><VoteWritten /></div>
+
+        <!-- <div                                          
+          v-for="(item, idx) in list"
+          v-bind:item="item"
+          v-bind:key="idx"
+          style=""
+        >
+          <TextRadio :idx="idx" />
+        </div> -->
+
       </div>
-      <div name="vote-end-button">
-        <button>투표 확인</button>
-        <button>투표 종료</button>
+      <div name="vote-end-button" style="margin-top:-10px;margin-bottom:20px">
+        <center style="margin-bottom:50px">
+          <a href="#" class="button">투표 하기!</a>
+        </center>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import VoteCard from "@/components/votepage/VoteCard";
-import VoteWritten from "@/components/votepage/VoteWritten";
-import axios from "axios";
+import VoteCard from '@/components/votepage/VoteCard';
+import VoteWritten from '@/components/votepage/VoteWritten';
+import ImageRadio from '@/components/votepage/ImageRadio';
+import TextRadio from '@/components/votepage/TextRadio';
+import axios from 'axios';
 const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 
 export default {
   components: {
     VoteCard,
     VoteWritten,
+    ImageRadio,
+    TextRadio,
+  },
+  data: function() {
+    return {
+      list: ['일', '이', '삼'],
+    };
   },
   created() {
     this.getContractAddress();
@@ -78,5 +101,47 @@ export default {
 };
 </script>
 
-<style >
+<style>
+.wrap {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -86px;
+  margin-left: -89px;
+  text-align: center;
+}
+
+a {
+  -webkit-transition: all 200ms cubic-bezier(0.39, 0.5, 0.15, 1.36);
+  -moz-transition: all 200ms cubic-bezier(0.39, 0.5, 0.15, 1.36);
+  -ms-transition: all 200ms cubic-bezier(0.39, 0.5, 0.15, 1.36);
+  -o-transition: all 200ms cubic-bezier(0.39, 0.5, 0.15, 1.36);
+  transition: all 200ms cubic-bezier(0.39, 0.5, 0.15, 1.36);
+  display: block;
+  margin: 20px auto;
+  max-width: 180px;
+  text-decoration: none;
+  border-radius: 4px;
+  padding: 20px 30px;
+}
+
+a.button {
+  color: rgba(30, 22, 54, 0.6);
+  box-shadow: rgba(30, 22, 54, 0.4) 0 0px 0px 2px inset;
+}
+
+a.button:hover {
+  color: rgba(255, 255, 255, 0.85);
+  box-shadow: rgba(30, 22, 54, 0.7) 0 0px 0px 40px inset;
+}
+
+a.button2 {
+  color: rgba(30, 22, 54, 0.6);
+  box-shadow: rgba(30, 22, 54, 0.4) 0 0px 0px 2px inset;
+}
+
+a.button2:hover {
+  color: rgba(255, 255, 255, 0.85);
+  box-shadow: rgba(30, 22, 54, 0.7) 0 80px 0px 2px inset;
+}
 </style>
