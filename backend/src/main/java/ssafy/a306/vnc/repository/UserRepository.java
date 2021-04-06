a147fa6fc2658feaf5e18b1a72eff7b1b78a5686
ljeehwan <ljeehwan@naver.com>
2021-04-06 15:53:47 +0900

    fix: backend 소나큐브이슈 수정

--- backend/src/main/java/ssafy/a306/vnc/repository/UserRepository.java
+++ backend/src/main/java/ssafy/a306/vnc/repository/UserRepository.java
@@ -3,11 +3,11 @@ package ssafy.a306.vnc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import ssafy.a306.vnc.entity.UserVo;
+import ssafy.a306.vnc.entity.UserEntity;
 import ssafy.a306.vnc.model.UserDto;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserVo,Long> {
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
 	boolean existsByuserEmail(String userEmail);
-	UserVo findByuserEmail(String userEmail);
+	UserEntity findByuserEmail(String userEmail);
 }
