89794a2d780831391429508700e75b3786988ee4
KSJ <khb3221@naver.com>
2021-04-08 01:39:51 +0900

    feat: 시작날짜 - 종료날짜, 진행중/마감 뱃지 추가
    feat: 시작날짜 - 종료날짜, 진행중/마감 뱃지 추가

--- frontend/src/views/VoteGraph.vue
+++ frontend/src/views/VoteGraph.vue
@@ -2,13 +2,33 @@
   <div>
     <HNavGray />
     <div class="graph-container">
+      <b-modal
+        id="bv-modal-example2"
+        hide-header-close
+        hide-footer
+        no-close-on-backdrop
+      >
+        <template #modal-title>LOGIN</template>
+        <div style="text-align: center; font-family: sans-serif">
+          Login 후 투표결과를 확인할 수 있습니다.
+          <img src="@/assets/votelogo.png" />
+        </div>
+        <br />
+        <div class="d-block text-center justify-center">
+          <kakaoLogin />
+        </div>
+      </b-modal>
+
       <div class="graph-content1">
         <strong
           >Reliable <br />
           Vote.</strong
         >
         <h3>투표 결과를 확인해보세요!</h3>
-        <p id="votepage_desc" style="margin-left: 0px; margin-top: 0px;font-size: 27px;">
+        <p
+          id="votepage_desc"
+          style="margin-left: 0px; margin-top: 0px;font-size: 27px;"
+        >
           블록체인을 적용한 신뢰성있는 통계 시스템! <br />눈으로 직접
           경험해보세요!
         </p>
@@ -16,18 +36,26 @@
       <div class="graph-content2">
         <h1 id="votepage_title">{{ mainTitle }}</h1>
         <!-- <h1>메인 제목</h1> -->
+        <div>
+          <b-badge variant="success" v-if="this.endDayCheck()">진행중</b-badge>
+          <b-badge variant="secondary" v-else>마감</b-badge>
+        </div>
         <div class="graph-content2-gul">
-        <img v-if="mainImagePath != ''" :src="mainImagePath" alt="" style="width: 400px; height: 300px; border-radius: 20px;margin-top: 30px" />
-        <p  id="votepage_desc">
-          {{ mainDescription }}
-        </p>
-        <hr style="width:80%"/>
-        <h1>투표 결과</h1>
-        <GChart type="BarChart" :data="chartData" :options="chartOptions" />
+          <img
+            v-if="mainImagePath != ''"
+            :src="mainImagePath"
+            alt=""
+            style="width: 400px; height: 300px; border-radius: 20px;margin-top: 30px"
+          />
+          <p id="votepage_desc">
+            {{ mainDescription }}
+          </p>
+          <hr style="width:80%" />
+          <h1>투표 결과</h1>
+          <GChart type="BarChart" :data="chartData" :options="chartOptions" />
         </div>
         <!-- <p>메인내용</p> -->
         <!-- <b-button>참가자 목록</b-button> -->
-        
       </div>
     </div>
   </div>
@@ -40,12 +68,14 @@ import { Utils } from '@/utils/index.js';
 import axios from 'axios';
 import { Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
+import kakaoLogin from '@/components/socialLogin/kakao.vue';
 const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 export default {
   components: {
     GChart,
     HNavGray,
     axios,
+    kakaoLogin,
   },
   data() {
     return {
@@ -53,12 +83,19 @@ export default {
       mainDescription: '',
       mainImagePath: '',
       imageExist: false,
-      colorIdx:0,
-      color:'',
+      colorIdx: 0,
+      color: '',
+      isLogin: false,
+      endedAt: '',
       // Array will be automatically processed with visualization.arrayToDataTable function
       loaded: false,
       chartData: [
-        ['Element', 'Density', { role: 'style' }, { role: 'annotation' }], /* 3번째옵션이 style 4번째옵션이 그래프위숫자표시 */
+        [
+          'Element',
+          'Density',
+          { role: 'style' },
+          { role: 'annotation' },
+        ] /* 3번째옵션이 style 4번째옵션이 그래프위숫자표시 */,
         ['Copper', 20, '#b87333', 1],
         ['Silver', 1, 'silver', 'Ag'],
         ['Gold', 1, 'gold', 'Au'],
@@ -70,34 +107,67 @@ export default {
           subtitle: 'Sales, Expenses, and Profit: 2014-2017',
         },
         /*  그래프 총넓이 */
-        backgroundColor:{
+        backgroundColor: {
           fill: '#e9ecef',
           // stroke : '#666',
           // strokeWidth : 5,
         },
         // width: 1000,
         height: 400,
-        legend: { position: 'top', maxLines: 4 }, /* 항목 위치 및 한줄에 최대4개  */
-        bar: { groupWidth: '75%' }, /* 막대그래프 넓이 */
+        legend: {
+          position: 'top',
+          maxLines: 4,
+        } /* 항목 위치 및 한줄에 최대4개  */,
+        bar: { groupWidth: '75%' } /* 막대그래프 넓이 */,
         isStacked: true,
-         hAxis: {
-            minValue: 0,
-            format: '0', /* 드디어찾았다... 소수점제거... */
-            gridlines: { count: 4 } ,/* 굵은 그리드 라인개수 */
-          },
+        hAxis: {
+          minValue: 0,
+          format: '0' /* 드디어찾았다... 소수점제거... */,
+          gridlines: { count: 4 } /* 굵은 그리드 라인개수 */,
+        },
       },
     };
   },
   async created() {
+    this.loginCheck();
     // 오류 발생 임시 주석 처리
-    this.test();
-    await this.getContractAddress();
-    // setInterval(() => {
-    //   this.chartData[1][1]++;
-    //   console.log(this.chartData);
-    // }, 1000);
+    if (this.isLogin == true) {
+      await this.getContractAddress();
+      this.subscribe();
+      // setInterval(() => {
+      //   this.chartData[1][1]++;
+      //   console.log(this.chartData);
+      // }, 1000);
+    }
+  },
+  mounted() {
+    if (this.isLogin == false) {
+      this.$bvModal.show('bv-modal-example2');
+    }
+    console.log('Test');
   },
   methods: {
+    endDayCheck() {
+      //지났음
+      if (this.endedAt * 1000 < Date.now() * 1) {
+        return false;
+      }
+      //안지났음
+      return true;
+    },
+    loginCheck() {
+      console.log(localStorage.getItem('access_token'));
+      console.log(localStorage.getItem('myData'));
+      if (
+        localStorage.getItem('access_token') == undefined ||
+        localStorage.getItem('myData') == undefined
+      ) {
+        console.log('로그인 안됨.');
+      } else {
+        console.log('로그인 됨.');
+        this.isLogin = true;
+      }
+    },
     async getContractAddress() {
       // console.log("true");
       this.$store.state.loading.text = '투표 주소를 가져오는 중입니다...';
@@ -128,27 +198,28 @@ export default {
       this.mainDescription = rs.description;
       this.mainImagePath = rs.imagePath;
       this.imageExist = rs.bImageExist;
+      this.endedAt = rs.endedAt;
       // set chart
-      this.chartData = [['Key', '득표 수',{ role: 'style'}]];
+      this.chartData = [['Key', '득표 수', { role: 'style' }]];
       // var idx = 0;
       rs.items.forEach((el) => {
-        console.log(el.title + '데이터 확인' + el.count + 'dd' + this.colorIdx );
+        console.log(el.title + '데이터 확인' + el.count + 'dd' + this.colorIdx);
         //var color = '';
-        if(this.colorIdx % 6 == 0){
-          this.color = '#BCE55C';// #BCE55C 연두
-        }else if(this.colorIdx%6 == 1){
-          this.color = '#A566FF';// #A566FF 보라
-        }else if(this.colorIdx%6 == 2){
-          this.color = '#5CD1E5';// #5CD1E5 하늘
-        }else if(this.colorIdx%6 == 3){
-          this.color = '#F361DC';// #F361DC 분홍
-        }else if(this.colorIdx%6 == 4){
-          this.color = '#E5D85C';// #E5D85C 노랑
-        }else if(this.colorIdx%6 == 5){
-          this.color = '#F15F5F';// #F15F5F 빨강
+        if (this.colorIdx % 6 == 0) {
+          this.color = '#BCE55C'; // #BCE55C 연두
+        } else if (this.colorIdx % 6 == 1) {
+          this.color = '#A566FF'; // #A566FF 보라
+        } else if (this.colorIdx % 6 == 2) {
+          this.color = '#5CD1E5'; // #5CD1E5 하늘
+        } else if (this.colorIdx % 6 == 3) {
+          this.color = '#F361DC'; // #F361DC 분홍
+        } else if (this.colorIdx % 6 == 4) {
+          this.color = '#E5D85C'; // #E5D85C 노랑
+        } else if (this.colorIdx % 6 == 5) {
+          this.color = '#F15F5F'; // #F15F5F 빨강
         }
 
-        this.chartData.push([el.title, el.count * 1,this.color]);
+        this.chartData.push([el.title, el.count * 1, this.color]);
         this.colorIdx++;
       });
 
@@ -175,7 +246,7 @@ export default {
       );
       chart.draw(data, options);
     },
-    test() {
+    subscribe() {
       const serverURL = 'http://localhost:8080/ws';
       let socket = new SockJS(serverURL);
       this.stompClient = Stomp.over(socket);
@@ -261,9 +332,9 @@ export default {
   text-align: center;
   width: 65%;
   background: #e9ecef;
-  padding-top:15px;
+  padding-top: 15px;
 }
-.graph-content2-gul{
-  min-height: 50% ;
+.graph-content2-gul {
+  min-height: 50%;
 }
 </style>
